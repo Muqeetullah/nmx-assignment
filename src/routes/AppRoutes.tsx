a2949fdfd7816ec31e4a8eee9e737f769c7c671d
Muqeet_ullah <muqeet.ullah@codistan.org>
2024-07-13 16:04:07 +0500

    Routes Updaetd

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,18 +6,20 @@ import {
   Navigate,
 } from "react-router-dom";
 import SignIn from "../containers/Auth/SignIn";
-import UserLisitng from "../containers/Admin/UserLisitng";
-import BookLisitng from "../containers/Admin/BookLisitng";
+
 import AddUser from "../containers/Admin/AddUser";
 import AddBook from "../containers/Admin/AddBook";
-import UsersWithBooksLisitng from "../containers/Shared/UsersWithBooksLisitng";
+
 import Layout from "../containers/Shared/Layout";
 import IssueBook from "../containers/User/IssueBook";
 import PrivateRoute from "./PrivateRoutes";
+import BookLisitng from "../containers/Admin/BookLisitng";
 import PublicRoutes from "./PublicRoutes";
 import UserDashboard from "../containers/User/UserDashboard";
 import AdminDashboard from "../components/AdminDashboard";
 import ViewProfile from "../components/ViewProfile";
+import UserListing from "../containers/Admin/UserLisitng";
+import UsersWithBooksListing from "../containers/Shared/UsersWithBooksLisitng";
 
 const AppRoutes: React.FC = () => {
   const jsonString = localStorage.getItem("user");
@@ -30,44 +32,50 @@ const AppRoutes: React.FC = () => {
       element: (
         <PrivateRoute>
           <Layout>
+            {role === "admin" ? <AdminDashboard /> : <UserDashboard />}
+            <Outlet />
+          </Layout>
+        </PrivateRoute>
+      ),
+    },
+    {
+      path: "/admin",
+      element: (
+        <PrivateRoute>
+          <Layout>
             <Outlet />
           </Layout>
         </PrivateRoute>
       ),
       children: [
+        { path: "view-user-list", element: <UserListing /> },
+        { path: "view-book-list", element: <BookLisitng /> },
+        { path: "add-user", element: <AddUser /> },
+        { path: "add-book", element: <AddBook /> },
         {
-          index: true,
-          element: role === "admin" ? <AdminDashboard /> : <UserDashboard />,
-        },
-        {
-          path: "admin",
-          children: [
-            { path: "view-user-list", element: <UserLisitng /> },
-            { path: "view-book-list", element: <BookLisitng /> },
-            { path: "add-user", element: <AddUser /> },
-            { path: "add-book", element: <AddBook /> },
-            {
-              path: "profile",
-              element: (
-                <ViewProfile
-                  fullName={jsonObject.name}
-                  email={jsonObject.email}
-                  phoneNumber="1234567890"
-                  address="123 Main St, Anytown USA"
-                />
-              ),
-            },
-          ],
-        },
-
-        {
-          path: "view-issued-book-list",
-          children: [
-            { index: true, element: <UsersWithBooksLisitng /> },
-            { path: "issue-book", element: <IssueBook /> },
-          ],
+          path: "profile",
+          element: (
+            <ViewProfile
+              fullName={jsonObject.name}
+              email={jsonObject.email}
+              phoneNumber="1234567890"
+              address="123 Main St, Anytown USA"
+            />
+          ),
         },
       ],
+    },
+    {
+      path: "/view-issued-book-list",
+      element: (
+        <PrivateRoute>
+          <Layout>
+            <UsersWithBooksListing />
+          </Layout>
+          <Outlet />
+        </PrivateRoute>
+      ),
+      children: [{ path: "issue-book", element: <IssueBook /> }],
     },
     {
       path: "/login",
